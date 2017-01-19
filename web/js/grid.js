a4e06e1bd93dc16227a2d56e60540126d931ca74
EGOmaniack <EGOmaniack@mail.ru>
2017-01-20 08:45:59 +1000

    cells

--- web/js/grid.js
+++ web/js/grid.js
@@ -1,6 +1,6 @@
 $(document).ready(
     function(){
-        var cells = "<div class='cell'></div>";
+        var cells = "";
         //$(".canvas").html(cells + cells);
         for(var i = 0;i < 20; i++){
             for(var j = 0;j<9;j++){
@@ -9,5 +9,18 @@ $(document).ready(
         }
         $(".canvas").html(cells);
         //$(".cell").css("position" , "absolute");
+
+        $(".cell").click(function(){
+            $(this).toggleClass("flip");
+           var canvcells =  $(".cell");
+           //console.log(canvcells.length);
+           var yelcells = 0;
+           for(var i =0;i<canvcells.length;i++){
+               if(canvcells[i].className == "cell flip")yelcells++;
+           }
+           //console.log(yelcells);
+           $("#selected").html(yelcells);
+        });
     }
 );
+
