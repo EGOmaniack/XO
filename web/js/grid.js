7672e4b8a3b309fbdbe3aa579d175c6eacaa7ec1
EGOmaniack <EGOmaniack@mail.ru>
2017-01-20 08:55:29 +1000

    убрал коментарии

--- web/js/grid.js
+++ web/js/grid.js
@@ -1,14 +1,12 @@
 $(document).ready(
     function(){
         var cells = "";
-        //$(".canvas").html(cells + cells);
         for(var i = 0;i < 20; i++){
             for(var j = 0;j<9;j++){
                 cells = cells + "<div class='cell'></div>";
             }
         }
         $(".canvas").html(cells);
-        //$(".cell").css("position" , "absolute");
 
         $(".cell").click(function(){
             $(this).toggleClass("flip");
